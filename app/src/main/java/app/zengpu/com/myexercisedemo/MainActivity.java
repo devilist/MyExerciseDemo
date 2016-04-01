722fe77fe9de32da65312d44f2526dc4ba6cf050
devilist <bit_zengpu@163.com>
2016-04-01 14:03:53 +0800

    Initial commit

---
+++ app/src/main/java/app/zengpu/com/myexercisedemo/MainActivity.java
@@ -0,0 +1,102 @@
+package app.zengpu.com.myexercisedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import app.zengpu.com.myexercisedemo.pull_refresh_load_0.RefreshAndLoadActivity;
+import app.zengpu.com.myexercisedemo.pull_refresh_load_1.RefreshAndLoadBaseActivity;
+import app.zengpu.com.myexercisedemo.tupianlunbo0.TuPianLunBoActivity;
+import app.zengpu.com.myexercisedemo.tupianlunbo1.ImageLoopActivity;
+
+/**
+ * Created by zengpu on 16/3/30.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button tuPianLunBo;
+    private Button imageLoop;
+    private Button refreshandLoad;
+    private Button refreshandLoad1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        tuPianLunBo = (Button) findViewById(R.id.go_to_tupianlunbo);
+        imageLoop = (Button) findViewById(R.id.go_to_image_loop);
+        refreshandLoad = (Button) findViewById(R.id.go_to_refresh_load);
+        refreshandLoad1 = (Button) findViewById(R.id.go_to_refresh_load1);
+
+        tuPianLunBo.setOnClickListener(this);
+        imageLoop.setOnClickListener(this);
+        refreshandLoad.setOnClickListener(this);
+        refreshandLoad1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.go_to_tupianlunbo:
+                Intent intent = new Intent(this, TuPianLunBoActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.go_to_image_loop:
+                Intent intent1 = new Intent(this, ImageLoopActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.go_to_refresh_load:
+                Intent intent2 = new Intent(this, RefreshAndLoadActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.go_to_refresh_load1:
+                Intent intent3 = new Intent(this, RefreshAndLoadBaseActivity.class);
+                startActivity(intent3);
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/app/zengpu/com/myexercisedemo/pull_refresh_load_0/RefreshAndLoadLayout.java
@@ -0,0 +1,184 @@
+package app.zengpu.com.myexercisedemo.pull_refresh_load_0;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import app.zengpu.com.myexercisedemo.R;
+
+/**
+ * Created by zengpu on 16/3/31.
+ */
+public class RefreshAndLoadLayout extends SwipeRefreshLayout
+                                    implements AbsListView.OnScrollListener {
+
+
+    /**
+     * 触发上拉操作的最小距离
+     */
+    private int mTouchSlop;
+
+    private ListView mListView;
+
+    /**
+     * 上拉监听器, 到了最底部的上拉加载操作
+     */
+    private OnLoadListener mOnLoadListener;
+
+    /**
+     * ListView的加载中footer
+     */
+    private View mListViewFooter;
+
+    /**
+     * 按下时的y坐标
+     */
+    private int mYDown;
+
+    /**
+     * 抬起时的y坐标, 与mYDown一起用于滑动到底部时判断是上拉还是下拉
+     */
+    private int mYMove;
+
+    /**
+     * 是否在加载中 ( 上拉加载更多 )
+     */
+    private boolean isLoading = false;
+
+
+    public RefreshAndLoadLayout(Context context) {
+        this(context,null);
+    }
+
+    public RefreshAndLoadLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mListViewFooter = LayoutInflater.from(context).inflate(R.layout.listview_footer,
+               null,false );
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        //初始化ListView
+
+        if (mListView == null) {
+
+            int childcount = getChildCount();
+            if (childcount >0) {
+                View childView = getChildAt(0);
+                if (childView instanceof ListView) {
+                    mListView = (ListView) childView;
+                    //设置滚动监听器给ListView, 使得滚动的情况下也可以自动加载
+                    mListView.setOnScrollListener(this);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mYDown = (int) ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mYMove = (int) ev.getRawY();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (canLoad()) {
+                    loadData();
+                }
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 是否可以加载更多, 条件是到了最底部, listview不在加载中, 且为上拉操作.
+     * @return
+     */
+    private boolean canLoad() {
+        return isBottom() && isPullUp() && !isLoading;
+    }
+
+    /**
+     * 判断是否到了最底部
+     * @return
+     */
+    private boolean isBottom() {
+        if (mListView != null && mListView.getAdapter() != null) {
+            return mListView.getLastVisiblePosition() ==
+                    (mListView.getAdapter().getCount() - 1);
+        }
+        return false;
+    }
+
+    /**
+     * 是否是上拉操作
+     * @return
+     */
+    private boolean isPullUp() {
+        return (mYDown - mYMove) >= mTouchSlop;
+    }
+
+    /**
+     * 如果到了最底部,而且是上拉操作.那么执行onLoad方法
+     */
+    private void loadData() {
+        if (mOnLoadListener != null) {
+            setLoading(true);
+            mOnLoadListener.onLoad();
+        }
+    }
+
+    /**
+     *
+     * @param loading
+     */
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+        if (isLoading) {
+            mListView.addFooterView(mListViewFooter);
+        } else {
+            mListView.removeFooterView(mListViewFooter);
+            mYDown = 0;
+            mYMove = 0;
+        }
+    }
+
+    public void setmOnLoadListener(OnLoadListener loadListener) {
+        mOnLoadListener = loadListener;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                         int totalItemCount) {
+        if (canLoad()) {
+            // 滚动时到了最底部也可以加载更多
+            loadData();
+        }
+    }
+
+    /**
+     * 加载更多的监听器
+     */
+    public static interface OnLoadListener {
+        public void onLoad();
+    }
+}
