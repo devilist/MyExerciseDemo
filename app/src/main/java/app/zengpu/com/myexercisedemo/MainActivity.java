4a8345d5b2cd3dd14f012c6d9508926adb87ad1f
devilist <bit_zengpu@163.com>
2016-08-02 00:28:49 +0800

    APST

--- app/src/main/java/app/zengpu/com/myexercisedemo/MainActivity.java
+++ app/src/main/java/app/zengpu/com/myexercisedemo/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import app.zengpu.com.myexercisedemo.MainActivity.RecyclerViewAdapter.OnItemClickListener;
 import app.zengpu.com.myexercisedemo.Utils.CustomAlertDialog;
+import app.zengpu.com.myexercisedemo.demolist.advancepagerslidingtabstrip.activity.ApstActivity;
 import app.zengpu.com.myexercisedemo.demolist.galleryfinaldemo.GalleryFinalActivity;
 import app.zengpu.com.myexercisedemo.demolist.multi_drawer.MultiDrawerActivity;
 import app.zengpu.com.myexercisedemo.demolist.photoloop0.PhotoLoopActivity;
@@ -94,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         demoList.add(new String[]{"下拉刷新，上拉加载", RefreshAndLoadActivity.class.getName()});
         demoList.add(new String[]{"GalleryFinal图片查看器", GalleryFinalActivity.class.getName()});
         demoList.add(new String[]{"视频断点拍摄", VideoAppendActivity.class.getName()});
+        demoList.add(new String[]{"APST", ApstActivity.class.getName()});
     }
 
     public static class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
--- extras/galleryfinal/src/main/java/cn/finalteam/galleryfinal/utils/Utils.java
+++ extras/galleryfinal/src/main/java/cn/finalteam/galleryfinal/utils/Utils.java
@@ -32,7 +32,7 @@ public class Utils {
 
     public static String getFileName(String pathandname) {
         int start = pathandname.lastIndexOf("/");
-        int end = pathandname.lastIndexOf(".");
+        int end = pathandname.lastIndexOf("");
         if (start != -1 && end != -1) {
             return pathandname.substring(start + 1, end);
         } else {
